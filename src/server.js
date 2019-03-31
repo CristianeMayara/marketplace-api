d86033ea341eea844e7368e7b7b98a33a19b315b
Cristiane Mayara <cris.mayara@gmail.com>
2019-03-31 16:53:57 -0300

    Exception handling

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const validate = require('express-validation')
 const databaseConfig = require('./config/database')
 
 class App {
@@ -10,6 +11,7 @@ class App {
     this.database()
     this.middlewares()
     this.routes()
+    this.exception()
   }
 
   database () {
@@ -25,6 +27,18 @@ class App {
 
   routes () {
     this.express.use(require('./routes'))
+  }
+
+  exception () {
+    this.express.use((err, req, res, next) => {
+      if (err instanceof validate.ValidationError) {
+        return res.status(err.status).json(err)
+      }
+
+      return res
+        .status(err.status || 500)
+        .json({ error: 'Internal Server Error' })
+    })
   }
 }
 
