ee8c5a859a4a30cbdefe27330447a5c6ae83fcfa
Cristiane Mayara <cris.mayara@gmail.com>
2019-03-31 22:33:23 -0300

    Add and store Purchase model

---
+++ src/app/models/Purchase.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const PurchaseSchema = new mongoose.Schema({
+  ad: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Ad',
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = mongoose.model('Purchase', PurchaseSchema)
